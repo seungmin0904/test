e6207b730ea4984009ce88d7be95c9b6eb72edc9
seungmin <gogo90490@gmail.com>
2025-03-24 11:33:06 +0900

    reset 연습

---
+++ Number.java
@@ -0,0 +1,5 @@
+public class Number {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("asd");
+        System.out.println("aslkdn");
+        System.out.println("aslkdnal");
+        System.out.println("ladknslk");
+    }
+}
